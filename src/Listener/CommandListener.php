60c69c2f1316502c048a56e45661795f854f4b73
morontt <morontt@gmail.com>
2015-11-17 00:13:39 +0200

    check requirements

--- src/Listener/CommandListener.php
+++ src/Listener/CommandListener.php
@@ -13,6 +13,18 @@ class CommandListener
      */
     public function beforeRun(ConsoleCommandEvent $event)
     {
+        if (!function_exists('gzencode')) {
+            $event->getOutput()->writeln('');
+            $event->getOutput()->writeln('Install and enable the <comment>Zlib</comment> extension.');
+            exit(1);
+        }
+
+        if (!function_exists('openssl_random_pseudo_bytes')) {
+            $event->getOutput()->writeln('');
+            $event->getOutput()->writeln('Install and enable the <comment>OpenSSL</comment> extension.');
+            exit(1);
+        }
+
         $command = $event->getCommand();
 
         try {
